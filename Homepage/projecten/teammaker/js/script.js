db60db7fcdaf108918ed15eaaac32b067cc39782
DeviN11111 <70692338+DeviN11111@users.noreply.github.com>
2021-03-30 19:44:39 +0200

    szdfzdfdfs

--- Homepage/projecten/teammaker/js/script.js
+++ Homepage/projecten/teammaker/js/script.js
@@ -3,31 +3,35 @@ var input = document.getElementById("input1");
 var team1 = document.getElementById('team1');
 var team2 = document.getElementById('team2');
 var names;
-var allowed = true;
+
 
 submit.onclick = function(){
 	names = input.value.split("\n");
-	checkInput()
-	if(allowed == true){
+	
+
+	if(checkInput()){
 		createElem()
+		createTeamsMethod2()
+
 
 	}
 	else {
 		////// WARNING MSG
-
+		console.log("I found cheese")
 	}
-	
-
 }
+
 function checkInput(){
+	var allowed = true;
 	for(i=0; i<names.length; i++){
 		names[i] = names[i].trim();
-		if(names[i] == "" || names.length%2 != 0 || names.length > 10) {
-			allowed = false;
-			console.log("I found cheese")
+		if(names[i] == "" || names.length%2 != 0 || names.length > 10 || names.length == 2) {
+			allowed = false;	
 		}
 	}
+	return allowed
 }
+
 function createElem(){
 	for(i=0; i<(names.length / 2); i++){
 		var elem = document.createElement("li");
@@ -38,5 +42,31 @@ function createElem(){
 		var elem = document.createElement("li");
 		team2.appendChild(elem);
 		elem.id = "team2persoon"+(i+1);
+	}
+}
+
+function createTeamsMethod1(){
+	var persoonIdTeam1 =0;
+	var persoonIdTeam2=0;
+	for(i=0; i<names.length; i++){
+		if(i%2 == 0){
+			persoonIdTeam1 = persoonIdTeam1 + 1
+			document.getElementById('team1persoon'+(persoonIdTeam1)).innerHTML = names[i]
+			console.log('team1persoon'+(i+1))
+		}
+		else {
+			persoonIdTeam2 = persoonIdTeam2 + 1
+			document.getElementById('team2persoon'+(persoonIdTeam2)).innerHTML = names[i]
+		}
+	}
+}
+function createTeamsMethod2(){
+	// maakt team 1
+	// names = [ "Devin", "Nick", "Koes", "Levi", "Kood", "kaser"]
+	var staticLength = names.length/2;
+	for (i=0; i<staticLength; i++) {
+		document.getElementById('team1persoon'+(i+1)).innerHTML = names[0]
+		names.splice(0, 1);
+		names.reverse();
 	}
 }
